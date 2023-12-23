dd8a9135253d10a3fd91e73f60f1f35982eb7205
yrhiba <yahyarhiba@gmail.com>
2023-12-23 09:22:19 +0100

    modified:   src/classes/Drone.cpp 	modified:   src/solutions/solution.cpp

--- src/classes/Drone.cpp
+++ src/classes/Drone.cpp
@@ -6,9 +6,7 @@
 Drone::Drone()
 {
 	this->light = 1;
-	// this->velocty = EVector(rand()%2000 - 1000, rand()%1000);
-	this->velocty = EVector(100, 0);
-	// this->velocty.setMag(600);
+	this->velocty = EVector(0, 0);
 	this->acceleration = EVector(0, 0);
 	this->maxSpeed = 600;
 }
@@ -20,15 +18,18 @@ void	Drone::applyForce(EVector force)
 
 void	Drone::edges(void)
 {
-	if (this->pos.x < 200 || this->pos.x > 9800)
+	if (this->pos.x <= 0 || this->pos.x >= 9999)
 	{
 		this->velocty.x *= -1;
 	}
 
-	if (this->pos.y < 200 || this->pos.y > 9800)
+	if (this->pos.y <= 0 || this->pos.y >= 9999)
 	{
 		this->velocty.y *= -1;
 	}
+
+	for (int i = 0; i < 2; i++)
+		this->updatePos();
 }
 
 void	Drone::updatePos(void)
--- src/solutions/solution.cpp
+++ src/solutions/solution.cpp
@@ -8,34 +8,18 @@
 
 void Game::solution()
 {
-	// cerr << "Visible Creatures: " << this->creature_visible_count << endl;
-	// cerr << "Visible Fishes: " << this->fishes_visible_count << endl;
-	// cerr << "Visible Uglys: " << this->uglys_visible_count << endl;
-
 	Actions actions;
 
 	for (auto &droneId : this->myDrones)
 	{
 		Drone &curDrone = this->getDroneById(droneId);
 
-		cerr << curDrone << endl;
-
-		cerr << "Pos: " << curDrone.pos << " " << curDrone.pos.magnitude() << endl;
-		cerr << "Vel: " << curDrone.velocty << " " << curDrone.velocty.magnitude() << endl;
-		cerr << "Acc: " << curDrone.acceleration << " " << curDrone.acceleration.magnitude() << endl;
-
 		// EXPLORE MODE
-		if (this->game_turn == 0)
-		{
-			curDrone.applyForce(EVector(0, 50));
-		}
 
-		curDrone.edges();
 		curDrone.updatePos();
+		curDrone.edges();
 
 		actions.moveToPos(curDrone.pos.x, curDrone.pos.y, curDrone.light);
-
-		cerr << endl;
 	}
 
 	return ;
