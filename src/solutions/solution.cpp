4e3f57e0f3c7a2aacf8126c702ea686fb4170219
yrhiba <yahyarhiba@gmail.com>
2023-12-29 23:04:31 +0100

    modified:   src/solutions/solution.cpp

--- src/solutions/solution.cpp
+++ src/solutions/solution.cpp
@@ -31,7 +31,7 @@ void Game::solution()
 			{
 				curDrone.single_line_path = SingleLinePath
 				(
-					EVector(5e3, 0),
+					EVector(2500, 0),
 					EVector(2000, MAP_SIZE - 1000), 
 					800
 				);
@@ -40,7 +40,7 @@ void Game::solution()
 			{
 				curDrone.single_line_path = SingleLinePath
 				(
-					EVector(5e3, 0),
+					EVector(7500, 0),
 					EVector(MAP_SIZE - 2000, MAP_SIZE - 1000),
 					800
 				);
@@ -75,3 +75,27 @@ void Game::solution()
 
 	return ;
 }
+
+/*
+Turn order taches:
+0 - simulate the already visible uglys moves (estimation : 99.9% work)
+1 - initialize the data from the previous turn (99.9%)
+2 - read the data of the current turn (100%)
+3 - calc the scores - availlable + possible (if i go to top) (10%)
+4 - drones missions. (bellow more detail availble)
+
+Drone: mission order
+1 - initialize the velocty and accleration by 0 : done (99.9% work)
+2 - remove from the map the fishes already scanned - TO-Do
+3 - calc the remaing availlable fishes to scan and there cost : TO-Do
+4 - define the target point (path) - calc the velocty : TO-Do
+5 - avoid the uglys with the calculated velocty : done (95% work fine)
+6 - algo for lighting the drone or not (depending if i am close enghouf to the target fishes or possible invisble ugly close to me).
+7 - update and output the move or the wait : done (100%)
+
+Other-Mission-possible-help:
+1 - using radar calc predicted target position for every Fish/Ugly.
+2 - calc the score i have currently in my drone and compare it to the opponents one
+	if it have the same creatures as mine go to top in the shortest movemment possible
+	to gain the bonus.
+*/
