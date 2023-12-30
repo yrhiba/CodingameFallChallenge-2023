f7e659e690ae1718547b8c1d98b8060aaf514d3c
yrhiba <yahyarhiba@gmail.com>
2023-12-30 18:33:08 +0100

    modified:   src/game-modules/fihes_targtet_point.cpp

--- src/game-modules/fihes_targtet_point.cpp
+++ src/game-modules/fihes_targtet_point.cpp
@@ -2,6 +2,41 @@
 
 /*start*/
 
+pair<EVector, EVector> calcTheIntersctionReactangle(vector<pair<EVector, EVector>> rectangles)
+{
+	if (rectangles.empty())
+	{
+		cerr << "No-information-for-that-fish." << endl;
+		return {EVector(0, 0), EVector(9999, 9999)};
+	}
+
+	double maxLeft = rectangles[0].first.x;
+	double maxTop = rectangles[0].first.y;
+	double minRight = rectangles[0].second.x;
+	double minBottom = rectangles[0].second.y;
+
+	// Iterate through the rectangles to find the intersection
+	for (const auto &rect : rectangles)
+	{
+		maxLeft = max(maxLeft, rect.first.x);
+		maxTop = max(maxTop, rect.first.y);
+		minRight = min(minRight, rect.second.x);
+		minBottom = min(minBottom, rect.second.y);
+	}
+
+	// Check if there is a valid intersection
+	if (maxLeft < minRight && maxTop < minBottom)
+	{
+		// Return the intersection rectangle
+		return {EVector(maxLeft, maxTop), EVector(minRight, minBottom)};
+	}
+	else
+	{
+		cerr << "there-is-something-wrong-with-radar." << endl;
+		return {EVector(0, 0), EVector(9999, 9999)};
+	}
+}
+
 void	Game::evaluate_fishes_targets(void)
 {
 	cerr << "Fishes To Scan: " << endl;
@@ -14,45 +49,17 @@ void	Game::evaluate_fishes_targets(void)
 		// skip the saved fishes
 		if (!fish.availlableToscan) continue;
 
-		cerr << fish << endl;
+		fish.existZone = calcTheIntersctionReactangle(fish.existZones);
+
+
+		EVector vect = fish.existZone.second - fish.existZone.first;
+
+		vect.setMag(calcDistance(fish.existZone.first, fish.existZone.second) / 2.0);
+
+		vect.roundme();
+
+		fish.targetPointToScan = fish.existZone.first + vect;
+
+		cerr << "fish:" << fish.id << " Target: " << fish.targetPointToScan << endl;
 	}
 }
-
-
-/*
-fish-type-1 => start(0, 2500) -> end(9999, 5000) -> Zone
-fish-type-2 => start(0, 5000) -> end(9999, 7500) -> Zone
-fish-type-2 => start(0, 7500) -> end(9999, 9999) -> Zone
-*/
-
-/*
-	TODO: Calculate the zone where the fish is.
-
-	Information i have:
-	------------------------------------
-	(fish.zone.start) -> (fish.zone.end)
-		+
-	for (drone : mydrones)
-	{
-		if (TL)
-		{
-			(0, 0) -> (drone.pos.x, drone.pos.y)
-		}
-
-		if (BR)
-		{
-			(drone.pos.x, drone.pos.y) -> (9999, 9999)
-		}
-
-		if (TR)
-		{
-			(drone.pos.x, drone.pos.y) -> (0, 9999)
-		}
-
-		if (BL)
-		{
-			(9999, 0) -> (drone.pos.x, drone.pos.y)
-		}
-	}
-	-----------------------------------------------
-*/
