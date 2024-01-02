8f55127f7ef837f3d5ce907bdcc2ab0944631491
yrhiba <yahyarhiba@gmail.com>
2024-01-02 12:32:06 +0100

    modified:   src/game-modules/game-setup/turn_setup.cpp

--- src/game-modules/game-setup/turn_setup.cpp
+++ src/game-modules/game-setup/turn_setup.cpp
@@ -1,16 +1,9 @@
-#include "../../header.hpp"
+#include "header.hpp"
 
 /*start*/
 
 void Game::clearGamePreviousTurnData(void)
 {
-	this->isScannedFish.clear();
-	this->isScannedByMeFish.clear();
-	this->isScannedByOpFish.clear();
-	this->isDronesScannedByMeFish.clear();
-	this->isDronesScannedByOpFish.clear();
-	this->isDeadFish.clear();
-
 	this->fishsPossibleToScan.clear();
 	this->fishsPossibleToKick.clear();
 	for (int type = 0; type <= 2; type++)
@@ -22,34 +15,22 @@ void Game::clearGamePreviousTurnData(void)
 
 void	Game::initializeAndUpdateTurnData(void)
 {
+	// initialize Game object variable
+	this->clearGamePreviousTurnData();
+
 	this->game_turn += 1;
 	this->creature_visible_count = 0;
-}
 
-void	Game::initializeAndUpdateFishesTurnData(void)
-{
+	// initialize + update fishes objects
 	for (auto &fish : this->allFishes)
 	{
-		if (fish.type == -1) continue;
+		fish.initializeAndSetupTurnData();
+	}
 
-		fish.availableToKick = true;
-		fish.availlableToscan = true;
-		fish.existZone = make_pair(EVector(-1, -1), EVector(-1, -1));
-		fish.targetPointToScan = EVector(-1, -1);
-		fish.existZones.clear();
-
-		// push back the fish zone depending on their type
-		if (fish.type == 0)
-		{
-			fish.existZones.push_back({EVector(0, 2500), EVector(9999, 5000)});
-		}
-		else if (fish.type == 1)
-		{
-			fish.existZones.push_back({EVector(0, 5000), EVector(9999, 7500)});
-		}
-		else if (fish.type == 2)
-		{
-			fish.existZones.push_back({EVector(0, 7500), EVector(9999, 9999)});
-		}
+	// TODO:
+	// initialize + update fishes objects
+	for (auto &drone : this->allDrones)
+	{
+		// drone.initializeAndSetupTurnData();
 	}
 }
