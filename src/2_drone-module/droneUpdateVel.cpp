c86343d224bc82004436bed2f26824a899369432
yrhiba <yahyarhiba@gmail.com>
2024-01-03 00:47:47 +0100

    modified:   src/2_drone-module/droneUpdateVel.cpp

--- src/2_drone-module/droneUpdateVel.cpp
+++ src/2_drone-module/droneUpdateVel.cpp
@@ -4,32 +4,30 @@
 
 void Game::droneUpdateVel(Drone &drone)
 {
-	// must go to scan a fish
-	if (drone.assignedFishToScan)
+	// must go to kick a fish out of the map : Highest Priority
+	if (drone.assignedFishToKick)
+	{
+		drone.action.setMsg("Kick-Fish-" + to_string(drone.TargetFishToKick));
+		this->droneUpdateVelToKickOutTheTargetFish(drone);
+	}
+	// must go to scan a fish : Second Priority
+	else if (drone.assignedFishToScan)
 	{
 		drone.action.setMsg("Scan-Fish-" + to_string(drone.TargetFishToScan));
 		this->droneUpdateVelToScanTheTargetFish(drone);
 	}
-	// go to save if i scan some fishes
+	// Go to save If Currently Sacnned fihes and don't have work to do
 	else if (drone.scannedCreatures.size() > 0)
 	{
 		// move to Top
 		drone.action.setMsg("Going-To-Save");
 		drone.velocty = EVector(0, -600);
 	}
-	// must go to kick a fish out of the map
-	else if (drone.assignedFishToKick)
-	{
-		drone.action.setMsg("Kick-Fish-" + to_string(drone.TargetFishToKick));
-		this->droneUpdateVelToKickOutTheTargetFish(drone);
-	}
 	// must go to top | save the scanned fishes
 	else
 	{
 		// Do Nothing
-		drone.action.setMsg("Wander-On-The-Map");
-		drone.velocty = drone.wanderForce();
-		drone.velocty.setMag(drone.maxSpeed);
-		drone.velocty.roundme();
+		drone.action.setMsg("Nothing-To-Do");
+		drone.velocty = EVector(0, 0);
 	}
 }
