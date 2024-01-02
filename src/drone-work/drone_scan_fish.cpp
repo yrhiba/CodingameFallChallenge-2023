b11758cdbf7a872876113903070fe0d62dcc087d
yrhiba <yahyarhiba@gmail.com>
2024-01-02 12:30:22 +0100

    modified:   src/drone-work/drone_scan_fish.cpp

--- src/drone-work/drone_scan_fish.cpp
+++ src/drone-work/drone_scan_fish.cpp
@@ -20,7 +20,7 @@ void	Game::droneUpdateVelToScanTheTargetFish(Drone &drone)
 	}
 	else
 	{
-		targetPos = targetFishToScan.targetPointToScan;
+		targetPos = targetFishToScan.estimationPosition;
 	}
 
 	targetPos.y += (700 * ((drone.pos.y <= targetPos.y) ? -1 : +0.5));
@@ -56,7 +56,7 @@ void	Game::dronesAssignFishToScanWithType(int fishesType)
 				if (!(this->fishsPossibleToScan.count(fishId))) continue;
 				Fish	&fish = this->getFishById(fishId);
 				if (!fish.availlableToscan) continue;
-				double	curDistance = calcDistance((fish.isVisible ? fish.pos : fish.targetPointToScan), drone.pos);
+				double	curDistance = calcDistance((fish.isVisible ? fish.pos : fish.estimationPosition), drone.pos);
 				if (((fishResId == -1) || (curDistance < fishResDis)))
 				{
 					fishResId = fish.id;
@@ -83,7 +83,7 @@ void	Game::dronesAssignFishToScanWithType(int fishesType)
 			{
 				Drone &drone = this->getDroneById(droneId);
 				if (drone.assignedFishToScan || drone.emergency) continue;
-				double	curDistance = calcDistance((fish.isVisible ? fish.pos : fish.targetPointToScan), drone.pos);
+				double	curDistance = calcDistance((fish.isVisible ? fish.pos : fish.estimationPosition), drone.pos);
 				if ((droneResId == -1) || (curDistance < droneResDis))
 				{
 					droneResId = drone.id;
