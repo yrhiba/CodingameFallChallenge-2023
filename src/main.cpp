048824edf7e99a0d472831dfc8506f9e09ec2368
yrhiba <yahyarhiba@gmail.com>
2024-01-02 22:26:55 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,49 +5,75 @@
 
 /*start*/
 
+/* intialize the game */
+Game game;
+
+void	GameSetup(void)
+{
+	/* read Creatures */
+	game.readSetupCreatures();
+}
+
+void	GameInitTurn(void)
+{
+	/* Initialize Data */
+	game.initTurn();
+}
+
+void	GameReadInput(void)
+{
+	/* read turn data */
+	game.readTurnData();
+}
+
+void	GameUpdateFishesDrones(void)
+{
+	/* fishes-updates-turn-data-and-set-flags*/
+	game.setFishesFlagsAndUpdates(); // estimated postion + availlavilty to scan + kick //
+
+	/* drones-updates-turn-data-and-set-flags*/
+	game.setDronesFlagsAndUpdates(); // check the mission state .. //
+}
+
+void	GameSimulation(void)
+{
+	// simulate the uglys //
+	game.uglysSimulation();
+	// simulate the fishes //
+	game.fishesSimulation();
+}
+
+void	GameOutputTurnActions(void)
+{
+	// start of new Turn //
+	// ################ //
+	#if DEBUG
+	cerr << "Game-Turn: " << game.game_turn << endl << endl;
+	#endif
+
+	// puts the solution //
+	game.solution();
+}
+
 int main()
 {
 	/* Initialize Random Generation */
 	srand(time(NULL));
 
-	/* intialize the game */
-	Game game;
-
-	/* read Creatures */
-	game.readSetupCreatures();
+	GameSetup();
 
 	/* Game Loop */
 	while (true) 
 	{
-		/* Initialize Data */
-		game.initTurn();
+		GameInitTurn();
 
-		/* read turn data */
-		game.readTurnData();
+		GameReadInput();
 
-		/* fishes-updates-turn-data-and-set-flags*/
-		game.setFishesFlagsAndUpdates(); // estimated postion + availlavilty to scan + kick //
+		GameUpdateFishesDrones();
 
-		/* drones-updates-turn-data-and-set-flags*/
-		game.setDronesFlagsAndUpdates(); // check the mission state .. //
+		GameSimulation();
 
-		// simulate the uglys //
-		game.uglysSimulation();
-
-		// simulate the fishes //
-		game.fishesSimulation();
-
-		// start of new Turn //
-		// ################ //
-		#if DEBUG
-		cerr << "Game-Turn: " << game.game_turn << endl << endl;
-		#endif
-
-		// puts the solution //
-		game.solution();
-
-		// end of The Turn //
-		// ################ //
+		GameOutputTurnActions();
 	}
 
 	return (0);
