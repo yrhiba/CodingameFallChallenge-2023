05bd39cb4cc9222abedafc2801f77a13e346bd25
yrhiba <yahyarhiba@gmail.com>
2024-01-02 12:12:29 +0100

    modified:   src/classes/Fish.cpp 	modified:   src/includes/Fish.hpp

--- src/classes/Fish.cpp
+++ src/classes/Fish.cpp
@@ -1,21 +1,174 @@
-#include "../header.hpp"
+#include "header.hpp"
 #include "../includes/Fish.hpp"
 
 /*start*/
 
-Fish::Fish():
-	id(-1), color(-1), type(-1), 
-	pos(-1, -1),
-	velocty(-1, -1),
-	scaned(0), dead(0), 
-	scannedByMe(0), scannedByOp(0),
-	isVisible(0), visibleAtTurn(-1)
+bool	Fish::isDeadNextTurn(void)
 {
+	EVector nextPos = this->pos + this->velocty;
+
+	if (nextPos.x < 0 || nextPos.x > 9999)
+	{
+		return true;
+	}
+
+	return false;
 }
 
-void Fish::setPos(EVector _pos)
+void	Fish::setNextPos(void)
 {
-	this->pos = _pos;
+	this->nextPos = this->pos + this->velocty;
+	this->snapToZone();
+}
+
+void	Fish::setStandardFishZone(void)
+{
+	if (this->type == -1)
+		this->existZones.push_back({EVector(0, 2500), EVector(9999, 9999)});
+	else if (this->type == 0)
+		this->existZones.push_back({EVector(0, 2500), EVector(9999, 5000)});
+	else if (this->type == 1)
+		this->existZones.push_back({EVector(0, 5000), EVector(9999, 7500)});
+	else if (this->type == 2)
+		this->existZones.push_back({EVector(0, 7500), EVector(9999, 9999)});
+}
+
+void	Fish::setAvaillabilty(void)
+{
+	if ((this->type == -1) || this->dead)
+	{
+		this->availableToKick = false;
+		this->availlableToscan = false;
+		return ;
+	}
+
+	if (this->scannedByMe || this->unsavedScanedByMe)
+	{
+		this->availlableToscan = false;
+	}
+	else
+	{
+		this->availlableToscan = true;
+	}
+
+	if (this->scannedByOp || this->unsavedScanedByOP)
+	{
+		this->availableToKick = false;
+	}
+	else
+	{
+		this->availableToKick = true;
+	}
+}
+
+void	Fish::snapToZone(void)
+{
+	if (this->existZones.empty())
+		return ;
+
+	int minx, maxx;
+	int miny, maxy;
+
+	minx = this->existZones.front().first.x;
+	miny = this->existZones.front().first.y;
+	maxx = this->existZones.front().second.x;
+	maxy = this->existZones.front().second.y;
+
+	if (this->pos.x < minx)
+		this->pos.x = minx;
+
+	if (this->pos.x > maxx)
+		this->pos.x = maxx;
+
+	if (this->pos.y < miny)
+		this->pos.y = miny;
+
+	if (this->pos.y > maxy)
+		this->pos.y = maxy;
+}
+
+void	Fish::pushExistZone(EVector pos, string radar)
+{
+	// push back the fish zone depending on direction given by radar
+	if (radar == "TL")
+	{
+		this->existZones.push_back({EVector(0, 0), pos});
+	}
+	else if (radar == "TR")
+	{
+		this->existZones.push_back({EVector(pos.x, 0), EVector(9999, pos.y)});
+	}
+	else if (radar == "BR")
+	{
+		this->existZones.push_back({pos, EVector(9999, 9999)});
+	}
+	else if (radar == "BL")
+	{
+		this->existZones.push_back({EVector(0, pos.y), EVector(pos.x, 9999)});
+	}
+}
+
+void	Fish::extimatePossiblePosition(void)
+{
+	if ((this->type != -1) && (this->dead || (this->existZones.size() != 3)))
+	{
+		this->dead = true;
+		this->estimationPosition = EVector(-1, -1);
+		return;
+	}
+
+	this->existZone = calcTheIntersctionReactangle(this->existZones);
+
+	EVector vect = this->existZone.second - this->existZone.first;
+
+	vect.setMag(calcDistance(this->existZone.first, this->existZone.second) / 2.0);
+
+	vect.roundme();
+
+	this->estimationPosition = this->existZone.first + vect;
+}
+
+void	Fish::initializeSetupGameData(void)
+{
+	this->id = 0;
+	this->color = 0;
+	this->type = 0;
+	this->pos = EVector(0, 0);
+	this->velocty = EVector(0, 0);
+	this->nextPos = EVector(0, 0);
+	this->scaned = false;
+	this->dead = false;
+	this->deadNextTurn = false;
+	this->scannedByMe = false;
+	this->scannedByOp = false;
+	this->unsavedScanedByMe = false;
+	this->unsavedScanedByOP = false;
+	this->isVisible = false;
+	this->visibleAtTurn = -1;
+	this->availableToKick = true;
+	this->availlableToscan = true;
+	this->dronesScan = set<int>();
+	this->myDronesScan = set<int>();
+	this->opDronesScan = set<int>();
+	this->existZone = make_pair(EVector(0, 0), EVector(1e4, 1e4));
+	this->existZones = vector< pair<EVector, EVector> >();
+	this->estimationPosition = EVector(0, 0);
+}
+
+void	Fish::initializeAndSetupTurnData(void)
+{
+	this->scaned = false;
+	this->scannedByMe = false;
+	this->scannedByOp = false;
+	this->unsavedScanedByMe = false;
+	this->unsavedScanedByOP = false;
+	this->dronesScan = set<int>();
+	this->myDronesScan = set<int>();
+	this->opDronesScan = set<int>();
+	this->existZone = make_pair(EVector(0, 0), EVector(1e4, 1e4));
+	this->existZones = vector< pair<EVector, EVector> >();
+	this->estimationPosition = EVector(0, 0);
+	this->setStandardFishZone();
 }
 
 /* Fish Comparison Operators OverLoads */
--- src/includes/Fish.hpp
+++ src/includes/Fish.hpp
@@ -16,23 +16,50 @@ struct Fish
 	/*fish-flags*/
 	int		scaned; // UPDATED EACH TURN
 	int		dead; // UPDATED EACH TURN
+	int		deadNextTurn; // UPDATED EACH TURN
 	int		scannedByMe; // UPDATED EACH TURN
 	int		scannedByOp; // UPDATED EACH TURN
+	int		unsavedScanedByMe; // UPDATED EACH TURN
+	int		unsavedScanedByOP; // UPDATED EACH TURN
 	int		isVisible; // UPDATED EACH TURN
 	int		visibleAtTurn; // UPDATED EACH TURN
 	bool	availableToKick; // UPDATED EACH TURN
 	bool	availlableToscan; // UPDATED EACH TURN
 	/*fish-flags*/
 
+	/*drones-currently-scan-the-fish*/
+	set<int>	dronesScan; // UDPATED EACH TURN
+	set<int>	myDronesScan; // UDPATED EACH TURN
+	set<int>	opDronesScan; // UDPATED EACH TURN
+	/*drones-currently-scan-the-fish*/
+
 	/*for-fishes-evaluations*/
 	pair<EVector, EVector>				existZone; // UPDATED EACH TURN
 	vector< pair<EVector, EVector> >	existZones; // UPDATED EACH TURN
-	EVector								targetPointToScan; // UPDATED EACH TURN
+	EVector								estimationPosition; // UPDATED EACH TURN
 	/*for-fishes-evaluations*/
 
+	/*snap-fish-into-fihsType-Zone*/
+	void	snapToZone(void);
+	/*snap-fish-into-fihsType-Zone*/
+
 	/*sters*/
-	void	setPos(EVector _pos);
+	void	setNextPos(void);
+	void	setStandardFishZone(void);
+	void	setAvaillabilty(void);
 	/*sters*/
+
+	/*Push-Exist-Zone*/
+	void	pushExistZone(EVector pos, string radar);
+	/*Push-Exist-Zone*/
+
+	/*check-dead-for-next-turn*/
+	bool	isDeadNextTurn(void);
+	/*check-dead-for-next-turn*/
+
+	/*estimate-possible-position*/
+	void	extimatePossiblePosition(void);
+	/*estimate-possible-position*/
 
 	/*operators-overlod*/
 	bool operator<(const Fish &other) const;
@@ -42,9 +69,12 @@ struct Fish
 	bool operator==(const Fish &other) const;
 	/*operators-overlod*/
 
-	/*constructor*/
-	Fish();
-	/*constructor*/
+	/*initialize-setup-turn*/
+	void	initializeSetupGameData(void);
+	/*initialize-setup-turn*/
+	/*initialize-each-turn*/
+	void	initializeAndSetupTurnData(void);
+	/*initialize-each-turn*/
 };
 
 istream &operator>>(istream &is, Fish &fish);
