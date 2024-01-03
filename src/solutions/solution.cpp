30b15819d6c2449ba77d92304efb45ed58d13017
yrhiba <yahyarhiba@gmail.com>
2024-01-03 01:10:02 +0100

    modified:   src/solutions/solution.cpp

--- src/solutions/solution.cpp
+++ src/solutions/solution.cpp
@@ -31,11 +31,13 @@ void Game::solution()
 		}
 		else
 		{
+			// updatint the target position of the drone
+			// this->droneUpdateTarget(curDrone);
 			// update mission and velocty
 			this->droneUpdateVel(curDrone);
-			// avoid uglys
+			// update the velocty if uglys/edges coillision detected
 			this->dronesAvoidnes(curDrone);
-			// light desion on/off
+			// light desion on/off based on target fishes to scan and invisible uglys estimation distance + battery state.
 			this->droneLighEvaluateState(curDrone);
 			// update position
 			curDrone.updatePos();
@@ -53,38 +55,3 @@ void Game::solution()
 	#endif
 }
 
-/*
-To-Work-On:
-0 - score the current fishes scan for me and opponents
-1 - remove fish from the map
-
-Tips:
-0 - don't wast time with the leaderBoard
-	the game still not finish yet and play for
-	the best bot you code ever, see the details.
-
-Turn order taches:
-0 - simulate the already visible uglys moves (estimation : 99.9% work)
-1 - initialize the data from the previous turn (99.9%)
-2 - read the data of the current turn (100%)
-3 - calc the scores - availlable + possible (if i go to top) (10%)
-4 - drones missions. (bellow more detail availble)
-
-Drone: mission order
-1 - initialize the velocty and accleration by 0 : done (99.9% work)
-2 - remove from the map the fishes already scanned - TO-Do
-3 - calc the remaing availlable fishes to scan and there cost : TO-Do
-4 - define the target point (path) - calc the velocty : TO-Do
-5 - avoid the uglys with the calculated velocty : done (99.9% work fine) 
-6 - algo for lighting the drone or not (depending if i am close enghouf to the target 
-	fishes or possible invisble ugly close to me).
-7 - update and output the move or the wait : done (100%)
-
-Other-Mission-possible-help:
-1 - using radar calc predicted target position for every Fish/Ugly.
-2 - calc the score i have currently in my drone and compare it to the opponents one
-	if it have the same creatures as mine go to top in the shortest movemment possible
-	to gain the bonus.
-3 - add if i already scanned the fish and my opponents gonna lose some points
-	sifte fish bera map.
-*/
