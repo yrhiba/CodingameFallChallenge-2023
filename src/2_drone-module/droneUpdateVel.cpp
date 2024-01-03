c1c3cbcb531e2bcc3d34ce8d8e8dcf1e7ab1b825
yrhiba <yahyarhiba@gmail.com>
2024-01-03 01:10:17 +0100

    modified:   src/2_drone-module/droneUpdateVel.cpp

--- src/2_drone-module/droneUpdateVel.cpp
+++ src/2_drone-module/droneUpdateVel.cpp
@@ -4,8 +4,21 @@
 
 void Game::droneUpdateVel(Drone &drone)
 {
+	/*
+		have the highest highest priority and if it true
+		that mean that's the last mission that i need to do
+		to win the game.
+		set it true after evaluated the possibilty	of winning
+		without wasting time with other missions 
+	*/
+	if (drone.mustGoToTop)
+	{
+		// move to Top
+		drone.action.setMsg("Going-To-Win");
+		drone.velocty = EVector(0, -600);
+	}
 	// must go to kick a fish out of the map : Highest Priority
-	if (drone.assignedFishToKick)
+	else if (drone.assignedFishToKick)
 	{
 		drone.action.setMsg("Kick-Fish-" + to_string(drone.TargetFishToKick));
 		this->droneUpdateVelToKickOutTheTargetFish(drone);
