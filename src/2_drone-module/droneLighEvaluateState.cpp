63382b6929a067858dea63d851d4a499b0c23146
yrhiba <yahyarhiba@gmail.com>
2024-01-04 23:56:39 +0100

    modified:   src/2_drone-module/droneLighEvaluateState.cpp 	modified:   src/2_drone-module/dronesAssingFishesToScan.cpp

--- src/2_drone-module/droneLighEvaluateState.cpp
+++ src/2_drone-module/droneLighEvaluateState.cpp
@@ -4,8 +4,40 @@
 
 void	Game::droneLighEvaluateState(Drone &drone)
 {
-	if (this->game_turn > 4 && drone.battery >= 5 && drone.pos.y > 3000)
+	int visibleUglys = 0;
+	int totalUglys = this->typeFishes[-1].size();
+	for (int ulgyId : this->typeFishes[-1])
+		visibleUglys+=(this->getFishById(ulgyId).visibleAtTurn!=-1);
+	if (totalUglys == visibleUglys)
 	{
-		drone.light = (this->game_turn % 2) ? 1 : 0;
+		if (drone.assignedFishToKick)
+		{
+			drone.light = false;
+			return ;
+		}
+		if (!drone.assignedFishToScan)
+		{
+			drone.light = false;
+			return ;
+		}
+		for (int fishId : this->fishsPossibleToScan)
+		{
+			Fish &fish = this->getFishById(fishId);
+			double curDis = calcDistance(fish.estimationPosition, drone.pos);
+			if (curDis < 2200)
+			{
+				drone.light = true;
+				return ;
+			}
+		}
+		drone.light = false;
+		return ;
+	}
+	else
+	{
+		if (this->game_turn > 4 && drone.battery >= 5 && drone.pos.y > 2500)
+		{
+			drone.light = ((rand()%10+1)%3==0);
+		}
 	}
 }
