45f6f9c3d1899ed36c53ae4b611b1c8c90249234
yrhiba <yahyarhiba@gmail.com>
2024-01-06 02:55:45 +0100

    modified:   src/2_drone-module/droneLighEvaluateState.cpp

--- src/2_drone-module/droneLighEvaluateState.cpp
+++ src/2_drone-module/droneLighEvaluateState.cpp
@@ -4,16 +4,13 @@
 
 void	Game::droneLighEvaluateState(Drone &drone)
 {
-	if ((drone.battery < 5) || (this->game_turn < 5) || (drone.pos.y < 3000)
+	if ((drone.battery < 5) || (this->game_turn < 3) || (drone.pos.y < 3000)
 		|| (drone.mustGoToTop) || (drone.emergency) || (drone.assignedFishToKick))
 	{
 		drone.light = false;
 		return ;
 	}
 
-	int	r = 3;
 
-	if (drone.pos.y > 6500) r = 2;
-
-	drone.light = (this->game_turn%r==0);
+	drone.light = (this->game_turn%2);
 }
